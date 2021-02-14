d673f83773863a2ebe9c493bbc440ebd36f9dd91
Lenildo Luan <lenildoluan@gmail.com>
2021-02-14 16:27:23 -0300

    feat: add label for input

---
+++ main.js
@@ -0,0 +1,43 @@
+const mainForm =  document.getElementById('mainForm');
+
+mainForm.addEventListener('submit', function(element){
+    const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    const requestUrl = 'https://ml-page-api.herokuapp.com/api/v1/lenildo/users';
+    const mainForm = {
+        nameInput: document.getElementById('formName'),
+        emailInput: document.getElementById('formEmail'),
+        checkbox: document.getElementById('checkbox'),
+        checkboxSpan: document.getElementById('checkboxSpan'),
+    }   
+    const requestBody = {
+        name: mainForm.nameInput.value,
+        email: mainForm.emailInput.value,
+    }
+
+    element.preventDefault();
+
+    if(requestBody.name == null || !nameRegex.test(requestBody.name)) {
+        alert(requestBody.name);
+        mainForm.nameInput.style.borderColor = '#D50000';
+    } else if(requestBody.email == null || !emailRegex.test(requestBody.email)) {
+        alert('EMAIL DEU RUIM');
+        mainForm.nameInput.style.borderColor = '#262626';
+        mainForm.emailInput.style.borderColor = '#D50000';
+    } else if(!mainForm.checkbox.checked) {
+        mainForm.emailInput.style.borderColor = '#262626';
+        mainForm.checkboxSpan.style.borderColor = '#D50000';
+        // alert('NUM TA CHECKADO PAI');
+    } else {
+        alert('DEU BOM');
+        mainForm.checkboxSpan.style.borderColor = '#262626';
+        // fetch(requestUrl, {
+        //     method: 'post',                                              
+        //     body: 'requestBody',
+        // }).then(response => {
+        //     console.log(response);
+        // }).catch(err => {
+        //     console.log(err);
+        // })
+    }
+})
