7bf151f9149151d7e054cd6555ca82ae7d536621
Lenildo Luan <lenildoluan@gmail.com>
2021-02-14 19:08:55 -0300

    fix: remove alerts

--- main.js
+++ main.js
@@ -18,18 +18,14 @@ mainForm.addEventListener('submit', function(element){
     element.preventDefault();
 
     if(requestBody.name == null || !nameRegex.test(requestBody.name)) {
-        alert(requestBody.name);
         mainForm.nameInput.style.borderColor = '#D50000';
     } else if(requestBody.email == null || !emailRegex.test(requestBody.email)) {
-        alert('EMAIL DEU RUIM');
         mainForm.nameInput.style.borderColor = '#262626';
         mainForm.emailInput.style.borderColor = '#D50000';
     } else if(!mainForm.checkbox.checked) {
         mainForm.emailInput.style.borderColor = '#262626';
         mainForm.checkboxSpan.style.borderColor = '#D50000';
-        // alert('NUM TA CHECKADO PAI');
     } else {
-        alert('DEU BOM');
         mainForm.checkboxSpan.style.borderColor = '#262626';
         // fetch(requestUrl, {
         //     method: 'post',                                              
