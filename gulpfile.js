0a785a4a5ec9ea06cce3a2d514c428bd48a4e32a
Lenildo Luan <lenildoluan@gmail.com>
2021-02-24 11:41:57 -0300

    feat: building project with gulp

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const gulp = require('gulp');
+const useref = require('gulp-useref');
+const uglify = require('gulp-uglify');
+const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const gulpIf = require('gulp-if');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
+
+const config = {
+    baseDir: 'src',
+    htmlDir: 'src/**/*.html',
+    cssDir: 'src/styles/**/*.css',
+    jsDir: 'src/scripts/**/*.js'
+}
+
+gulp.task('server', () => {
+    // Init locar server
+    browserSync.init({
+        server: {
+            baseDir: config.baseDir
+        },
+    })
+
+    // Reloads the browser whenever HTML, CSS or JS files change
+    gulp.watch([config.htmlDir, config.cssDir, config.jsDir], (cb) => {
+        browserSync.reload();
+        cb();
+    });
+});
+
+gulp.task('js:build', () => {
+    return gulp.src('src/**/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('css:build', () => {
+    return gulp.src('src/**/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.css', autoprefixer()))
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images:build', () => {
+    return gulp.src('src/assets/**/*.+(png|jpg|gif|svg)')
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('dist/assets'))
+});
+
+gulp.task('clean:dist', () => {
+    return del.sync('dist');
+})
+
+gulp.task('cache: clear', () => {
+    return cache.clearAll()
+})
+
+gulp.task('build', () => {
+    return gulp.src('src/**/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.html', htmlmin({ collapseWhitespace: true })))
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', autoprefixer()))
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'))
+});
