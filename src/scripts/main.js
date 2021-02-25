0a904512660e000067a5f615eec070c7c181d746
Lenildo Luan <lenildoluan@gmail.com>
2021-02-25 00:14:47 -0300

    Fix: input dont get value

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,15 +12,15 @@ mainForm.addEventListener('submit', function(element) {
     }
     const requestBody = {
         api_key: 'iE5kRmVMSkzt2K9H4_FAnA',
-        first_name: mainForm.nameInput,
-        email: mainForm.emailInput,
+        first_name: mainForm.nameInput.value,
+        email: mainForm.emailInput.value,
     }
 
     element.preventDefault();
 
-    if (mainForm.nameInput == null || !nameRegex.test(mainForm.nameInput)) {
+    if (requestBody.first_name == null || !nameRegex.test(requestBody.first_name)) {
         mainForm.nameInput.style.borderColor = '#D50000';
-    } else if (mainForm.emailInput == null || !emailRegex.test(mainForm.emailInput)) {
+    } else if (requestBody.email == null || !emailRegex.test(requestBody.email)) {
         mainForm.nameInput.style.borderColor = '#262626';
         mainForm.emailInput.style.borderColor = '#D50000';
     } else if (!mainForm.checkbox.checked) {
